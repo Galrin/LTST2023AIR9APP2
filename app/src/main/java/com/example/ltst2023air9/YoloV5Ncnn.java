887010e5442810c6c41de9135cc17cf57ebf3606
Alexander Post <elemental.gm@gmail.com>
2023-06-10 19:32:30 +0300

    java prepare

--- app/src/main/java/com/example/ltst2023air9/YoloV5Ncnn.java
+++ app/src/main/java/com/example/ltst2023air9/YoloV5Ncnn.java
@@ -2,6 +2,8 @@ package com.example.ltst2023air9;
 
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 
 public class YoloV5Ncnn
 {
@@ -9,7 +11,6 @@ public class YoloV5Ncnn
         System.loadLibrary("yolov5ncnn");
     }
     public native boolean Init(AssetManager assetManager);
-
     public class Obj
     {
         public float x;
@@ -18,9 +19,13 @@ public class YoloV5Ncnn
         public float h;
         public String label;
         public float prob;
+
+        public String room_label;
+        public float room_prob;
     }
 
     public native Obj[] Detect(Bitmap bitmap, boolean use_gpu);
 
 
 }
+
